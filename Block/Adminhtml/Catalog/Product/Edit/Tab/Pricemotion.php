3ca28dcc4fdfdf6bf8d0063438f8e163cda738c9
Albert Peschar <albert@peschar.net>
2020-07-03 11:32:43 +0300

    Add widget IFrame

--- Block/Adminhtml/Catalog/Product/Edit/Tab/Pricemotion.php
+++ Block/Adminhtml/Catalog/Product/Edit/Tab/Pricemotion.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pricemotion\Magento2\Block\Adminhtml\Catalog\Product\Edit\Tab;
 
+use Magento\Csp\Model\Collector\DynamicCollector;
+use Magento\Csp\Model\Policy\FetchPolicy;
 use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Model\Product;
@@ -12,18 +14,50 @@ class Pricemotion extends Template {
     public function __construct(
         Template\Context $context,
         Registry $coreRegistry,
+        DynamicCollector $csp,
         array $data = []
     ) {
         $this->coreRegistry = $coreRegistry;
+        $csp->add(new FetchPolicy('frame-src', false, [$this->getAppUrl()]));
         parent::__construct($context, $data);
     }
 
     protected function _beforeToHtml() {
         $this->assign('settings', [
+            'app_url' => $this->getAppUrl(),
             'ean' => $this->getProduct()->getData($this->getEanAttribute()),
+            'token' => $this->getApiToken(),
         ]);
 
         return parent::_beforeToHtml();
+    }
+
+    private function getApiToken(): ?string {
+        if (!$this->getApiKey()) {
+            return null;
+        }
+
+        $expiresAt = time() + 3600;
+
+        return $this->base64encode(implode('', [
+            hash('sha256', $this->getApiKey(), true),
+            hash_hmac('sha256', $expiresAt, $this->getApiKey(), true),
+            pack('P', $expiresAt),
+        ]));
+    }
+
+    private function base64encode(string $data): string {
+        $result = base64_encode($data);
+        $result = rtrim($result, '=');
+        $result = strtr($result, [
+            '+' => '-',
+            '/' => '_',
+        ]);
+        return $result;
+    }
+
+    private function getApiKey(): ?string {
+        return $this->_scopeConfig->getValue('pricemotion/general/api_key');
     }
 
     private function getProduct(): Product {
@@ -32,5 +66,15 @@ class Pricemotion extends Template {
 
     private function getEanAttribute(): ?string {
         return $this->_scopeConfig->getValue('pricemotion/attributes/ean');
+    }
+
+    public function getViewFileUrl($fileId, array $params = []) {
+        $url = parent::getViewFileUrl($fileId, $params);
+        $url .= '?=' . uniqid(); // TODO: Only during development, otherwise append version
+        return $url;
+    }
+
+    private function getAppUrl() {
+        return 'http://localhost:8562'; // TODO: Change for production
     }
 }
--- view/adminhtml/web/js/pricemotion.js
+++ view/adminhtml/web/js/pricemotion.js
@@ -1,1 +1,42 @@
-alert(1);
+function Pricemotion(rootSelector) {
+    const ATTR_SETTINGS = 'data-settings';
+
+    const root = document.querySelector(rootSelector);
+    if (!root) {
+        throw new Error(`No element matched selector: ${rootSelector}`);
+    }
+
+    if (!root.hasAttribute(ATTR_SETTINGS)) {
+        throw new Error(`Root element does not have ${ATTR_SETTINGS} attribute`);
+    }
+
+    const settings = JSON.parse(root.getAttribute('data-settings'));
+
+    const frame = document.createElement('iframe');
+    frame.classList.add('pricemotion-frame');
+    frame.src = settings.app_url + '/integration/widget?' + buildQuery({
+        token: settings.token,
+        ean: settings.ean
+    });
+    frame.style.width = '100%';
+    frame.style.border = '0';
+    frame.style.height = '0';
+
+    root.appendChild(frame);
+
+    window.addEventListener('message', function (e) {
+        if (e.origin !== settings.app_url) {
+            return;
+        }
+        var message = JSON.parse(e.data);
+        if (message.type === 'setWidgetHeight') {
+            frame.style.height = message.value + 'px';
+        }
+    });
+
+    function buildQuery(query) {
+        return Object.entries(query).map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`).join('&');
+    }
+}
+
+new Pricemotion('#pricemotion');
