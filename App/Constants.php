af1519c2d539db7d977a52726031bc79e2a5f301
Albert Peschar <albert@peschar.net>
2020-09-02 12:48:59 +0300

    Read version from composer installed.json

--- App/Constants.php
+++ App/Constants.php
@@ -3,21 +3,44 @@ namespace Pricemotion\Magento2\App;
 
 class Constants {
 
-    const VERSION = '$VERSION$';
+    private static $version = '$VERSION$';
+
     const ATTR_LOWEST_PRICE = 'pricemotion_lowest_price';
     const ATTR_LOWEST_PRICE_RATIO = 'pricemotion_lowest_price_ratio';
     const ATTR_UPDATED_AT = 'pricemotion_updated_at';
     const ATTR_SETTINGS = 'pricemotion_settings';
 
-    public static function isDevelopmentVersion() {
-        return self::VERSION == '$VER' . 'SION$';
-    }
-
     public static function getAssetVersion() {
         if (self::isDevelopmentVersion()) {
             return $_SERVER['REQUEST_TIME'];
+        } elseif (self::$version != '$VER' . 'SION$') {
+            return self::$version;
         } else {
-            return self::VERSION;
+            return self::$version = self::getComposerVersion();
+        }
+    }
+
+    private static function getComposerVersion() {
+        $manifest = __DIR__ . '/../../../composer/installed.json';
+        if (!is_file($manifest)) {
+            return null;
+        }
+        $contents = file_get_contents($manifest);
+        if ($contents === false) {
+            return null;
+        }
+        $data = json_decode($contents, true);
+        if (!is_array($data)) {
+            return null;
+        }
+        foreach ($data as $package) {
+            if ($package['name'] !== 'pricemotion/module-pricemotion') {
+                continue;
+            }
+            if (empty($package['version'])) {
+                continue;
+            }
+            return preg_replace('/^v/', '', $package['version']);
         }
     }
 
@@ -27,6 +50,10 @@ class Constants {
         } else {
             return 'https://www.pricemotion.nl/app';
         }
+    }
+
+    private static function isDevelopmentVersion() {
+        return !empty($_SERVER['PRICEMOTION_DEVELOPMENT']);
     }
 
 }
