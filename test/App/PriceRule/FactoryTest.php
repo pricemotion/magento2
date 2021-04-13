f7fc54051a0c0889a7715e17ab984ca9c4bc17d4
Albert Peschar <albert@peschar.net>
2021-04-13 16:52:56 +0300

    Add some more tests for price rules

--- test/App/PriceRule/FactoryTest.php
+++ test/App/PriceRule/FactoryTest.php
@@ -1,12 +1,44 @@
 <?php
 namespace Pricemotion\Magento2\App\PriceRule;
 
+use DOMDocument;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Pricemotion\Magento2\App\Product;
 
 class FactoryTest extends TestCase {
     public function testDisabled() {
-        $factory = new Factory;
+        $factory = new Factory();
         $rule = $factory->fromArray(['rule' => 'disabled']);
         $this->assertInstanceOf(Disabled::class, $rule);
+    }
+
+    public function testMissingParameter() {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Required parameter missing for rule: percentageBelowAverage');
+        (new Factory())->fromArray(['rule' => 'percentageBelowAverage']);
+    }
+
+    public function testPercentageBelowAverage() {
+        $rule = (new Factory())->fromArray([
+            'rule' => 'percentageBelowAverage',
+            'percentageBelowAverage' => 10,
+        ]);
+        $this->assertInstanceOf(PercentageBelowAverage::class, $rule);
+        $document = new DOMDocument();
+        $document->loadXML("<?xml version='1.0'?>
+            <response>
+                <info>
+                    <price>
+                        <min>0</min>
+                        <max>0</max>
+                        <avg>100.10</avg>
+                    </price>
+                </info>
+                <prices></prices>
+            </response>
+        ");
+        $product = Product::fromXmlResponse($document);
+        $this->assertEquals(90.09, $rule->calculate($product));
     }
 }
