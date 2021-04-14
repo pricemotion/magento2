1e6fba954b11fc95fa13277a9c7a779921440677
Albert Peschar <albert@peschar.net>
2021-04-14 14:25:28 +0300

    Fix error during clean install

--- Console/Command/Update.php
+++ Console/Command/Update.php
@@ -2,9 +2,10 @@
 namespace Pricemotion\Magento2\Console\Command;
 
 use Magento\Framework\App\Area;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\State;
 use Monolog\Handler\StreamHandler;
-use Pricemotion\Magento2\Cron\Update as CronUpdate;
+use Pricemotion\Magento2\Cron;
 use Pricemotion\Magento2\Logger\Logger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputDefinition;
@@ -13,19 +14,15 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Update extends Command {
-    private $job;
-
     private $state;
 
     private $logger;
 
     public function __construct(
         string $name,
-        CronUpdate $job,
         State $state,
         Logger $logger
     ) {
-        $this->job = $job;
         $this->state = $state;
         $this->logger = $logger;
         parent::__construct($name);
@@ -41,15 +38,16 @@ class Update extends Command {
     protected function execute(InputInterface $input, OutputInterface $output) {
         $this->state->setAreaCode(Area::AREA_CRONTAB);
         $this->logger->pushHandler(new StreamHandler(STDERR));
-        $this->job->setTimeLimit(null);
+        $job = ObjectManager::getInstance()->get(Cron\Update::class);
+        $job->setTimeLimit(null);
         if ($input->getOption('force')) {
-            $this->job->setIgnoreUpdatedAt(true);
+            $job->setIgnoreUpdatedAt(true);
         }
         if ($input->getOption('ean')) {
-            $this->job->setEanFilter($input->getOption('ean'));
+            $job->setEanFilter($input->getOption('ean'));
         }
         $this->logger->info(sprintf('Starting manual update run: %s', $input));
-        $this->job->execute();
+        $job->execute();
         $this->logger->info('Completed manual update run');
         return 0;
     }
