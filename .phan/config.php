3d03092cb45e19c562b980c53cc15cbe4a4f5b75
Albert Peschar <albert@peschar.net>
2021-04-16 19:26:21 +0300

    phan: Parse generated code

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,12 @@
 <?php
+$vendorDirectories = [
+    'www/vendor',
+    'www/generated',
+];
 
 return [
     'target_php_version' => '7.3',
-    'directory_list' => iterator_to_array((function () {
+    'directory_list' => iterator_to_array((function () use ($vendorDirectories) {
         foreach (scandir(__DIR__ . '/..') as $file) {
             if (is_dir(__DIR__ . '/../' . $file)
                 && preg_match('~^[A-Z]~', $file)
@@ -10,11 +14,9 @@ return [
                 yield $file;
             }
         }
-        yield 'www/vendor';
+        yield from $vendorDirectories;
     })(), false),
-    'exclude_analysis_directory_list' => [
-        'www/vendor',
-    ],
+    'exclude_analysis_directory_list' => $vendorDirectories,
     'plugins' => [
         'AlwaysReturnPlugin',
         'UnreachableCodePlugin',
