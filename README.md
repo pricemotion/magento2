fd105cefb500954c31d66c11f9358ccdf0f4ebfa
Albert Peschar <albert@peschar.net>
2022-07-27 16:26:54 +0300

    Add prettier pre-commit hook

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This extension integrates your Magento 2 store with
 [Pricemotion](https://pricemotion.nl).
 
-* [This extension on Magento
+- [This extension on Magento
   Marketplace](https://marketplace.magento.com/pricemotion-module-pricemotion.html)
-* [Information about this extension on Pricemotion.nl
+- [Information about this extension on Pricemotion.nl
   (Dutch)](https://www.pricemotion.nl/mogelijkheden/magento-2-extensie/)
-* [Installation & user
+- [Installation & user
   guide](https://docs.google.com/document/d/e/2PACX-1vS8n18Itwb_BeQw_7UvWTTOcpl_iXJLARzak6MPqX7JlNjGOoaS-WH9Cbyhroy3f9YInV3GbtvtsVSS/pub)
 
 We recommend installing the extension via GitHub, because Magento Marketplace
@@ -22,27 +22,27 @@ the extension from GitHub.
 Edit your `composer.json` and add the GitHub repository to the top of the
 `repositories` section:
 
-~~~
+```
 {
     "type": "vcs",
     "url": "https://github.com/pricemotion/magento2"
 },
-~~~
+```
 
 After you've done this, install or upgrade the extension using the command line:
 
-~~~
+```
 # If you have not yet installed the extension
 composer require pricemotion/module-pricemotion
-~~~
+```
 
-~~~
+```
 # If you have installed the extension previously
 composer update pricemotion/module-pricemotion
-~~~
+```
 
 Lastly run the Magento update process:
 
-~~~
+```
 bin/magento setup:upgrade
-~~~
+```
--- view/adminhtml/web/js/pricemotion.js
+++ view/adminhtml/web/js/pricemotion.js
@@ -1,106 +1,118 @@
 function Pricemotion(rootSelector) {
-    const root = document.querySelector(rootSelector);
-    if (!root) {
-        throw new Error(`No element matched selector: ${rootSelector}`);
+  const root = document.querySelector(rootSelector);
+  if (!root) {
+    throw new Error(`No element matched selector: ${rootSelector}`);
+  }
+
+  if (!root.dataset.settings) {
+    throw new Error(`Root element does not have settings data attribute`);
+  }
+
+  const settings = JSON.parse(root.dataset.settings);
+
+  const frame = document.createElement("iframe");
+  frame.src = settings.widget_url;
+  frame.style.width = "100%";
+  frame.style.border = "0";
+  frame.style.height = "250px";
+  root.appendChild(frame);
+
+  const form = createForm();
+  const inputs = {};
+
+  if (settings.form) {
+    setInput("form_key", settings.form_key);
+  }
+
+  let isValid;
+  let validatedResolvers = [];
+
+  window.addEventListener("message", function (e) {
+    if (e.origin !== settings.web_origin) {
+      return;
     }
-
-    if (!root.dataset.settings) {
-        throw new Error(`Root element does not have settings data attribute`);
+    const message = typeof e.data === "string" ? JSON.parse(e.data) : e.data;
+    if (message.type === "setWidgetHeight") {
+      frame.style.height = message.value + "px";
+    } else if (message.type === "updateProductSettings") {
+      isValid = message.isValid;
+      setInput("product.pricemotion_settings", JSON.stringify(message.value));
+      validatedResolvers.forEach(([resolve, reject]) =>
+        message.isValid ? resolve() : reject()
+      );
+      validatedResolvers = [];
     }
+  });
 
-    const settings = JSON.parse(root.dataset.settings);
-
-    const frame = document.createElement('iframe');
-    frame.src = settings.widget_url;
-    frame.style.width = '100%';
-    frame.style.border = '0';
-    frame.style.height = '250px';
-    root.appendChild(frame);
-
-    const form = createForm();
-    const inputs = {};
-
-    if (settings.form) {
-        setInput('form_key', settings.form_key);
+  document.body.addEventListener("click", function (e) {
+    if (e.target.closest(".pricemotion-submit")) {
+      e.preventDefault();
+      const valid = new Promise((resolve, reject) =>
+        validatedResolvers.push([resolve, reject])
+      );
+      valid.then(() => form.submit());
+      valid.catch(() => console.error("invalid"));
+      frame.contentWindow.postMessage(
+        { type: "validate" },
+        settings.web_origin
+      );
     }
+  });
 
-    let isValid;
-    let validatedResolvers = [];
+  document.body.addEventListener(
+    "click",
+    function (e) {
+      if (
+        e.target.closest(".save") &&
+        !e.target.closest(".pricemotion-submit") &&
+        isValid === false
+      ) {
+        e.stopImmediatePropagation();
 
-    window.addEventListener('message', function (e) {
-        if (e.origin !== settings.web_origin) {
-            return;
-        }
-        const message = typeof e.data === 'string' ? JSON.parse(e.data) : e.data;
-        if (message.type === 'setWidgetHeight') {
-            frame.style.height = message.value + 'px';
-        } else if (message.type === 'updateProductSettings') {
-            isValid = message.isValid;
-            setInput('product.pricemotion_settings', JSON.stringify(message.value));
-            validatedResolvers.forEach(([resolve, reject]) => message.isValid ? resolve() : reject());
-            validatedResolvers = [];
-        }
-    });
+        let delay = 0;
 
-    document.body.addEventListener('click', function (e) {
-        if (e.target.closest('.pricemotion-submit')) {
-            e.preventDefault();
-            const valid = new Promise((resolve, reject) => validatedResolvers.push([resolve, reject]));
-            valid.then(() => form.submit());
-            valid.catch(() => console.error("invalid"));
-            frame.contentWindow.postMessage(
-                {type: 'validate'},
-                settings.web_origin
-            );
-        }
-    });
+        try {
+          root
+            .closest("[data-index]")
+            .querySelector('[data-state-collapsible="closed"]')
+            .click();
+          delay = 250;
+        } catch (e) {}
 
-    document.body.addEventListener('click', function (e) {
-        if (e.target.closest('.save')
-            && !e.target.closest('.pricemotion-submit')
-            && isValid === false
-        ) {
-            e.stopImmediatePropagation();
+        setTimeout(() => {
+          root.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+          });
+        }, delay);
+      }
+    },
+    true
+  );
 
-            let delay = 0;
-
-            try {
-                root.closest('[data-index]').querySelector('[data-state-collapsible="closed"]').click();
-                delay = 250;
-            } catch (e) {}
-
-            setTimeout(() => {
-                root.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'center',
-                });
-            }, delay);
-        }
-    }, true);
-
-    function createForm() {
-        if (!settings.form) {
-            return root;
-        }
-        const form = document.createElement('form');
-        form.id = 'pricemotion-form';
-        form.action = settings.form.action;
-        form.method = 'post';
-        root.appendChild(form);
-        return form;
+  function createForm() {
+    if (!settings.form) {
+      return root;
     }
+    const form = document.createElement("form");
+    form.id = "pricemotion-form";
+    form.action = settings.form.action;
+    form.method = "post";
+    root.appendChild(form);
+    return form;
+  }
 
-    function setInput(name, value) {
-        if (!inputs[name]) {
-            const input = document.createElement('input');
-            input.name = name;
-            input.type = 'hidden';
-            input.dataset.formPart = 'product_form';
-            form.appendChild(input);
-            inputs[name] = input;
-        }
-        inputs[name].value = value;
+  function setInput(name, value) {
+    if (!inputs[name]) {
+      const input = document.createElement("input");
+      input.name = name;
+      input.type = "hidden";
+      input.dataset.formPart = "product_form";
+      form.appendChild(input);
+      inputs[name] = input;
     }
+    inputs[name].value = value;
+  }
 }
 
-new Pricemotion('#pricemotion');
+new Pricemotion("#pricemotion");
